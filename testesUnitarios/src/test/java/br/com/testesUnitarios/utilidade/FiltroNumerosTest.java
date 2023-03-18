32429f9171b06cb74b5955b340be553aee43ecd0
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2023-03-17 22:57:13 -0300

    Adicionando a Calsse FiltroNumeros e testando o metodo de numeros pares que fará um teste em uma lista de numeros

---
+++ testesUnitarios/src/test/java/br/com/testesUnitarios/utilidade/FiltroNumerosTest.java
@@ -0,0 +1,22 @@
+package br.com.testesUnitarios.utilidade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltroNumerosTest {
+
+    @Test
+    public void deveRetornarNumerosPares(){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        List<Integer> numerosEsperados = Arrays.asList(2, 4);
+        List<Integer> resultadoDoFiltro = FiltroNumeros.numerosPares(numeros);
+        Assertions.assertIterableEquals(numerosEsperados, resultadoDoFiltro);
+        
+    }
+
+}
