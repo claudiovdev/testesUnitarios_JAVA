f7b91b28a0cd46089993ee2ffd70457a7c8d9873
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2023-03-17 22:43:40 -0300

    Adicionando o pacote utilidade e adicionando classe SaudacaoUtil e criando o teste do metodo saudar

---
+++ testesUnitarios/src/test/java/br/com/testesUnitarios/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,17 @@
+package br.com.testesUnitarios.utilidade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaudacaoUtilTest {
+
+    @Test
+    public void saudar(){
+        String saudacao = SaudacaoUtil.saudar(9);
+        assertEquals("Bom dia", saudacao);
+    }
+
+
+}
