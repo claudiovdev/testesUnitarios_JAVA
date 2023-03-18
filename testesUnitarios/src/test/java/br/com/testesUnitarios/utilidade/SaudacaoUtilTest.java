646a745f4ce0b69f9c299bf3fd8d2048408202e4
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2023-03-17 23:13:35 -0300

    Desabilitando um teste e adicionando um comentario

--- testesUnitarios/src/test/java/br/com/testesUnitarios/utilidade/SaudacaoUtilTest.java
+++ testesUnitarios/src/test/java/br/com/testesUnitarios/utilidade/SaudacaoUtilTest.java
@@ -1,6 +1,7 @@
 package br.com.testesUnitarios.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +21,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @Disabled("Não é mais aplicavel")
     public void naoDeveLancarException(){
         assertDoesNotThrow(()-> SaudacaoUtil.saudar(0));
     }
